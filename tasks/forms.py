16550c5d62189376676b4ad78bf6fd40fe8d319e
Adrian <alextravc.342@gmail.com>
2023-12-24 18:54:48 -0500

    [ADD]: bootstrap styles

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,12 +1,30 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Task
 
 
 # ### Este form Solo crea los   Inputs
 
-class TaskForm(ModelForm): # inheritance ModelForm
+class TaskForm(forms.ModelForm): # inheritance ModelForm
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        # ## custom styles in inputs
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'important': forms.CheckboxInput(
+                attrs={
+                    'class': 'form-check-input'
+                }
+            ),
+        }
 
